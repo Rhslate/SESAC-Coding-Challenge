1edaf5dc1ac388d15e88ae5289c199ca1ce9b76a
Rhslate <84812018+Rhslate@users.noreply.github.com>
2024-05-16 10:12:34 -0700

    Add files via upload

---
+++ Calculating moves to rescue the Prince.py
@@ -0,0 +1,31 @@
+# Calculating moves to rescue the Princess
+# bot = m, Princess = p
+# bot is in center of grid
+
+#grid = []
+
+
+while m_pos != p_pos:
+    if m_pos[0] < p_pos[0]:
+       moves.append("UP")
+       m_pos = (m_pos[0] +1, m_pos[1])
+\n
+
+    elif m_pos[0] > p_pos[0]:
+         moves.append("DOWN")
+         m_pos = (m_pos[0] -1, m_pos[1])
+
+\n
+
+    if m_pos[1] < p_pos[1]:
+       moves.append("LEFT")
+       m_pos = (m_pos[0], bot_pos[1] +1)
+        
+\n
+
+    elif m_pos[1] > p_pos[1]:
+         moves.append("RIGHT")
+         m_pos = (m_pos[1], bot_pos[1] -1)
+        
+        
+print displayPathtoPrincess
