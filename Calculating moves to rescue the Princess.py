81223f7e9a7aaf78e8a02cbea6b89590ebc50a1d
Rhslate <84812018+Rhslate@users.noreply.github.com>
2024-05-17 15:17:42 -0700

    Add files via upload

---
+++ Calculating moves to rescue the Princess.py
@@ -0,0 +1,38 @@
+# Calculating moves to rescue the Princess
+# bot = m, Princess = p
+# bot is in center of grid
+# p_pos = princess position
+# m_pos = bot position
+
+def displayPathtoPrincess(n,grid):
+
+for i in range(n);
+    for j in range(n):
+        if grid[i][j] == 'm':
+            m_pos = (i, j)
+        elif grid[i][j] == 'p':
+            p_pos = (i, j)
+
+while m_pos != p_pos:
+    if m_pos[0] < p_pos[0]:
+       moves.append("UP")
+       m_pos = (m_pos[0] +1, m_pos[1])
+\n
+
+    elif m_pos[0] > p_pos[0]:
+         moves.append("DOWN")
+         m_pos = (m_pos[0] -1, m_pos[1])
+
+\n
+
+    if m_pos[1] < p_pos[1]:
+       moves.append("LEFT")
+       m_pos = (m_pos[0], bot_pos[1] +1)
+        
+\n
+
+    elif m_pos[1] > p_pos[1]:
+         moves.append("RIGHT")
+         m_pos = (m_pos[1], bot_pos[1] -1)
+
+displayPathtoPrincess(m, grid)
---
+++ Save princess 2.py
@@ -0,0 +1,36 @@
+#r = row
+#c = column
+#p = princess
+#m = bot
+
+def nextMove(N,r,c,grid):
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] == 'p':
+                p, m = i, j
+
+def UP():
+    print("UP")
+def DOWN():
+    print("DOWN")
+def LEFT():
+    print("LEFT")
+def RIGHT():
+    print("RIGHT")
+
+def r():
+    print(r)
+def c():
+    print(c)
+
+
+if r > p:
+    nextMove: "UP"
+if r <p:
+    nextMove: "DOWN"
+if c > m:
+    nextMove: "LEFT"
+if c < m:
+    nextMove: "RIGHT"
+
+nextMove(N,r,c,grid)
