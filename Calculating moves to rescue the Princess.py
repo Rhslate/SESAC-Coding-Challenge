bc6e372e89c7c1fe821e1c87ecd9ab184c3c86af
Rhslate <84812018+Rhslate@users.noreply.github.com>
2024-05-20 13:41:25 -0700

    Calculating moves to rescue the Princess.py
    I fixed my code to be object oriented

--- Calculating moves to rescue the Princess.py
+++ Calculating moves to rescue the Princess.py
@@ -1,40 +1,43 @@
-# Calculating moves to rescue the Princess
-# bot = m, Princess = p
-# bot is in center of grid
-# p_pos = princess position
-# m_pos = bot position
+def displayPathtoPrincess(n, grid):
+    # Initialize positions
+    m_pos = None
+    p_pos = None
 
-def displayPathtoPrincess(n,grid):
+    # Finding the positions of 'm' (bot) and 'p' (princess)
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 'm':
+                m_pos = (i, j)
+            elif grid[i][j] == 'p':
+                p_pos = (i, j)
 
-for i in range(n);
-    for j in range(n):
-        if grid[i][j] == 'm':
-            m_pos = (i, j)
-        elif grid[i][j] == 'p':
-            p_pos = (i, j)
+    # Initialize a list to store the moves
+    moves = []
 
-while m_pos != p_pos:
-    if m_pos[0] < p_pos[0]:
-       moves.append("UP")
-       m_pos = (m_pos[0] +1, m_pos[1])
-\n
-
-    elif m_pos[0] > p_pos[0]:
-         moves.append("DOWN")
-         m_pos = (m_pos[0] -1, m_pos[1])
-
-\n
-
-    if m_pos[1] < p_pos[1]:
-       moves.append("LEFT")
-       m_pos = (m_pos[0], bot_pos[1] +1)
+    # Generate moves to get from m_pos to p_pos
+    while m_pos != p_pos:
+        if m_pos[0] < p_pos[0]:
+            moves.append("DOWN")
+            m_pos = (m_pos[0] + 1, m_pos[1])
+        elif m_pos[0] > p_pos[0]:
+            moves.append("UP")
+            m_pos = (m_pos[0] - 1, m_pos[1])
         
-\n
+        if m_pos[1] < p_pos[1]:
+            moves.append("RIGHT")
+            m_pos = (m_pos[0], m_pos[1] + 1)
+        elif m_pos[1] > p_pos[1]:
+            moves.append("LEFT")
+            m_pos = (m_pos[0], m_pos[1] - 1)
 
-    elif m_pos[1] > p_pos[1]:
-         moves.append("RIGHT")
-         m_pos = (m_pos[1], bot_pos[1] -1)
+    # Print the moves
+    for move in moves:
+        print(move)
 
-displayPathtoPrincess(m, grid)
+if __name__ == "__main__":
+    n = int(input().strip())
+    grid = []
+    for _ in range(n):
+        grid.append(list(input().strip()))
 
-
+    displayPathtoPrincess(n, grid)
